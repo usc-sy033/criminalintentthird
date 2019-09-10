8694f528ab8529b2d608f5876a0aae46b5d4ad8e
usc-sy033 <s_y033@student.usc.edu.au>
2019-09-10 21:08:14 +1000

    chapter8, p164, inflating fragment changed to abstract lass, appcompat activity is superclass of the SingleFragmentActivity and CrimeActivity is now subclass of the SingleFragmentActivity.

---
+++ src/main/java/com/bignerdranch/android/criminalintentthird/CrimeLab.java
@@ -0,0 +1,49 @@
+package com.bignerdranch.android.criminalintentthird;
+/*singleton: private constructor and a get() method*/
+
+
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private List<Crime> mCrimes;
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 2 == 0); //Every other one
+            mCrimes.add(crime);
+        }
+    }
+
+    /*mCrimes 에 들어있는 값들을 하나씩 crime 에 다가 대입 */
+
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
